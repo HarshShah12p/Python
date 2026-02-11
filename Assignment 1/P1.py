55e6f4b5f3b62a54256685213fd30351af9bcd7c
HarshShah12p <102033372+HarshShah12p@users.noreply.github.com>
2026-02-11 22:28:54 +0530

    Add files via upload

---
+++ Assignment 1/P1.py
@@ -0,0 +1,15 @@
+f=(input("Enter the first number:"))
+
+s=(input("Enter the second number:"))
+
+Addition = int(f) + int(s)
+print("Addition:",Addition)
+
+Subtraction= int(f) - int(s)
+print("Subtraction:",Subtraction)
+
+Multiplication = int(f) * int(s)
+print("Multiplication:",Multiplication)
+
+Division = float(f) / float(s)
+print("Division:",Division)
---
+++ Assignment 1/P2.py
@@ -0,0 +1,5 @@
+FN=input("Enter your first name:")
+LN=input("Enter your last name:")
+FL=FN + " " + LN
+print("Hello," , FL , "!", "Welcome to Python programming" )
+
